dff0512a4388e89e756f2d527f71c47125d07bc7
Andrey3544 <box4435@mail.ru>
2022-12-17 15:14:31 +0300

    task47

---
+++ task47/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задайте двумерный массив (вручную) размером m×n, заполненный вещественными числами.
+Console.WriteLine("Введите размер массива числа m и n");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array2d = new double[m, n];
+Console.WriteLine("Введите элементы массива");
+for (int i = 0; i < m; i++)
+{
+
+    for (int j = 0; j < n; j++)
+    {
+        array2d[i, j] = Convert.ToDouble(Console.ReadLine());
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(+array2d[i, j] + " ");
+    }
+    Console.WriteLine();
+}
